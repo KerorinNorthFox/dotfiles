11f6e08b91974deb1577e863c157a27992dc754d
kerorinnf <kerorinnf@gmail.com>
2025-07-11 15:50:36 +0900

    シェル起動時に表示されるコマンドリストのフォーマットを調整

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -24,22 +24,25 @@ function fish_greeting
   echo ""
   echo "Hello "$USER"! Welcome to "(set_color cyan)"Fish Shell"(set_color normal)"! "
   echo "Version: "$version
-  printf "
-%sCommands%s:
-  %sGeneral%s:
+  echo ""
+  echo (set_color magenta)"Command List"(set_color normal)":"
+  printf "  %sGeneral%s:
     - z     : Move dir you often use.
-  %sI/O%s:
+" (set_color magenta) (set_color normal)
+  printf "  %sI/O%s:
     - less  : scrollable cat.
     - wc -l : Display line count of file.
     - df -h : Display the disk size.
     - du -h : Display the dir size.
-    - btm   : Display resource viewer. 
-  %sPackage%s:
+    - btm   : Display resource viewer.
+" (set_color magenta) (set_color normal)
+  printf "  %sPackage%s:
     - apt list -i : Display installed package.
     - apt list -u : Display upgradable package.
     - dpkg -l     : Display packages.
     - fisher      : Fish plugins manager.
-  "
+" (set_color magenta) (set_color normal)
+
 end
 
 ### Abbr, Alias and Command
