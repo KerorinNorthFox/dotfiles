ace46236073d1ad3eb6b30a778eb5efad0a86ed6
kerorinnf <kerorinnf@gmail.com>
2025-07-15 16:24:33 +0900

    init.luaにlazy.nvimがインストールされているか確認するコードを追記

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -18,7 +18,20 @@ if astronvim.default_colorscheme then
   end
 end
 
-require("lualine").setup()
-require("telescope").setup()
-vim.cmd[[colorscheme tokyonight-moon]]
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    vim.fn.system({
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch-stable",
+       lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- require("lualine").setup()
+-- require("telescope").setup()
+-- vim.cmd[[colorscheme tokyonight-moon]]
 
