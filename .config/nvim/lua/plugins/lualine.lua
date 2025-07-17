bf454c7e18ec4c37d8f290aa06f98cea63145eb5
kerorinnf <kerorinnf@gmail.com>
2025-07-17 09:09:21 +0900

    プラグイン追加

---
+++ .config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,8 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    component_separators = { left = "<", right = "<" },
+    section_separators = { left = "", right = "" },
+  },
+}
