c86c5dba8fbca9369309543705268e94c2dcda20
kerorinnf <kerorinnf@gmail.com>
2025-07-28 09:00:52 +0900

    neovimに言語別lspなどをインストールしようとしたけど失敗、保留中

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -7,6 +7,23 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
+  -- language packs
+  --  { import = "astrocommunity.pack.astro" },
+  { import = "astrocommunity.pack.fish" },
+  --  { import = "astrocommunity.pack.html-css" },
+  --  { import = "astrocommunity.pack.json" },
+  --  { import = "astrocommunity.pack.markdown" },
+  --  { import = "astrocommunity.pack.mdx" },
+  { import = "astrocommunity.pack.nim" },
+  -- { import = "astrocommunity.pack.python" },
+  --  { import = "astrocommunity.pack.sql" },
+  --  { import = "astrocommunity.pack.svelte" },
+  --  { import = "astrocommunity.pack.tailwindcss" },
+  --  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.typst" },
+  -- { import = "astrocommunity.pack.xml" },
+  -- { import = "astrocommunity.pack.yaml" },
+  -- others
   { import = "astrocommunity.bars-and-lines.lualine-nvim" },
   { import = "astrocommunity.bars-and-lines.dropbar-nvim" },
   { import = "astrocommunity.bars-and-lines.statuscol-nvim" },
