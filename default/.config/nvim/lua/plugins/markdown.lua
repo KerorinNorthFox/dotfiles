e77401a8a6d157a83015649350c467273df7dda6
kerorinnf <kerorinnf@gmail.com>
2025-12-05 13:44:25 +0900

    マークダウンプレビュー関係のプラグインを導入

---
+++ default/.config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,40 @@
+return {
+  -- markdownのプレビューを行う (on community.lua)
+  -- https://github.com/iamcco/markdown-preview.nvim
+  -- コマンド
+  -- :MarkdownPreview       # プレビューサーバー起動
+  -- :MarkdownPreviewStop   # プレビューサーバー停止
+  -- :MarkdownPreviewToggle # 起動/停止
+  -- { "iamcco/markdown-preview.nvim" },
+  {
+    "AstroNvim/astrocommunity",
+    { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+  },
+  -- markdownをレンダリングして表示する
+  -- https://github.com/MeanderingProgrammer/render-markdown.nvim
+  -- コマンド
+  -- :RenderMarkdown (enable) # 開始
+  -- :RenderMarkdown disable  # 停止
+  -- :RenderMarkdown toggle   # 開始/停止
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      code = {
+        sign = false,
+        width = "block",
+        right_pad = 1,
+      },
+      heading = {
+        sign = false,
+        icons = {},
+      },
+      checkbox = {
+        enabled = false,
+      },
+    },
+    ft = { "markdown" },
+    keys = {
+      { "<Space>Mm", ":RenderMarkdown toggle<CR>" },
+    },
+  },
+}
