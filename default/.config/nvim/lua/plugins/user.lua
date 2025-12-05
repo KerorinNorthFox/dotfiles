0146d2485a86ab373f8ad4294b4a58ca26690b5a
kerorinnf <kerorinnf@gmail.com>
2025-12-05 13:44:04 +0900

    ユーザー追加のプラグインはuser.luaに配置

--- default/.config/nvim/lua/plugins/user.lua
+++ default/.config/nvim/lua/plugins/user.lua
@@ -4,6 +4,60 @@
 
 ---@type LazySpec
 return {
+  -- 画面下部ステータスラインを追加
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons", lazy = true },
+    opts = {
+      component_separators = { left = "<", right = "<" },
+      section_separators = { left = "", right = "" },
+    },
+  },
+  -- neovimのコマンドラインを画面中央にフロートして表示 (on community.lua)
+  {
+    "folke/noice.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+    },
+    event = "VeryLazy",
+    opts = {
+      -- lsp = {
+      --   hover = {
+      --     enabled = false,
+      --   },
+      --   signature = {
+      --     enabled = false,
+      --   },
+      -- },
+    },
+  },
+  -- 括弧間やインデントを視覚的に表示
+  {
+    "shellRaining/hlchunk.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      chunk = {
+        enable = true,
+      },
+      indent = {
+        enable = true,
+      },
+      line_num = {
+        enable = true,
+      },
+    },
+  },
+  -- カーソルを滑らかに動かす
+  {
+    "sphamba/smear-cursor.nvim",
+    opts = {
+      stiffness = 0.8,
+      trailing_stiffness = 0.5,
+      distance_stop_animating = 0.5,
+      smear_between_buffers = true, -- Be smooth cursor when switching buffers.
+      -- scroll_buffer_space = true
+    },
+  },
   -- Snacks.nvim : 便利なプラグインまとめ
   {
     "folke/snacks.nvim",
@@ -36,7 +90,6 @@ return {
       },
     },
   },
-
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
   {
     "L3MON4D3/LuaSnip",
@@ -47,7 +100,6 @@ return {
       luasnip.filetype_extend("javascript", { "javascriptreact" })
     end,
   },
-
   {
     "windwp/nvim-autopairs",
     config = function(plugin, opts)
