ae9de53dffe7b50a089d0024256e8df2c0f67b87
kerorinnf <kerorinnf@gmail.com>
2025-12-05 14:01:48 +0900

    テーマをnordfoxに変更

--- default/.config/nvim/lua/plugins/theme.lua
+++ default/.config/nvim/lua/plugins/theme.lua
@@ -1,7 +1,10 @@
 return {
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  -- },
   {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
+    "EdenEast/nightfox.nvim",
   },
 }
