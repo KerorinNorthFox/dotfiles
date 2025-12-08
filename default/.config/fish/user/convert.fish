f8da67a452bcde155e0a66120f38100f31a57606
kerorinnf <kerorinnf@gmail.com>
2025-12-09 04:10:24 +0900

    convert.fishを変更. 変換処理に圧縮処理を含まれていたため削除. コピーだけに

--- default/.config/fish/user/convert.fish
+++ default/.config/fish/user/convert.fish
@@ -3,5 +3,10 @@ function convert
         echo "Usage: convert input_file output_file"
         return 1
     end
-    ffmpeg -i $argv[1] -c:v hevc_nvenc -preset p6 -rc vbr -f mp4 $argv[2]
+    
+    set -l input $argv[1]
+    set -l output $argv[2]
+    
+    ffmpeg -i $input -c:v copy -c:a copy -movflags +faststart $output
+    end
 end
