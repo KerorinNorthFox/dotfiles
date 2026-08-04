b4154339b99ea9bf77d068b2818f96102896218a
kerorinnf <kerorinnf@gmail.com>
2026-08-05 02:45:19 +0900

    feat: a new nixos configuration.

---
+++ nixos/flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
