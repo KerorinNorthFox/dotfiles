2b541163c04cee269660efcd08d8be475d6f77b8
kerorinnf <kerorinnf@gmail.com>
2026-08-06 22:54:22 +0900

    feat: neovimをモジュール化.

--- nixos/home/kerorinnf/home.nix
+++ nixos/home/kerorinnf/home.nix
@@ -8,6 +8,7 @@
 {
   imports = [
     ../../modules/user/buildtool/make.nix
+    ../../modules/user/editor/neovim
     ../../modules/user/shell/fish
     ../../modules/user/terminal/zellij
   ];
--- nixos/hosts/rossi/configuration.nix
+++ nixos/hosts/rossi/configuration.nix
@@ -68,7 +68,6 @@
 
     eza
     bat
-    neovim
 
   ];
 
