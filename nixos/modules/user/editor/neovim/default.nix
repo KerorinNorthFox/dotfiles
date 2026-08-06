6902c311211a1255532ea9f35e8dede3c8da2090
kerorinnf <kerorinnf@gmail.com>
2026-08-06 23:05:10 +0900

    fix: neovimをhome.packagesに追加する形にし, 設定は引き続き.config/nvimを使用する.

--- nixos/modules/user/editor/neovim/default.nix
+++ nixos/modules/user/editor/neovim/default.nix
@@ -1,3 +1,11 @@
 {
-  programs.neovim.enable = true;
+  pkgs,
+  ...
+}:
+
+{
+  home.packages = with pkgs; [ neovim ];
+  # TODO: 全ての設定完了後にシンボリックリンクを解除して設定ファイルのパスを設定.
+  # programs.neovim.enable = true;
+  # xdg.configFile."neovim".source = ../../../../../default/.config/nvim/init.lua;
 }
