bddfdddbdf147268dd1fe0458b8e496ab1940268
kerorinnf <kerorinnf@gmail.com>
2026-08-11 03:52:37 +0900

    feat(nixos/fcitx5): mozcをcurrnt input methodに追加するよう設定.

--- nixos/modules/system/common/fcitx5.nix
+++ nixos/modules/system/common/fcitx5.nix
@@ -11,10 +11,26 @@
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
-    fcitx5.waylandFrontend = true;
-    fcitx5.addons = with pkgs; [
-      fcitx5-mozc
-      fcitx5-gtk
-    ];
+
+    fcitx5 = {
+      waylandFrontend = true;
+      addons = with pkgs; [
+        fcitx5-mozc
+        fcitx5-gtk
+      ];
+      ignoreUserConfig = true;
+      settings.inputMethod = {
+        GroupOrder."0" = "Default";
+
+        "Group/0" = {
+          Name = "Default";
+          "Default Layout" = "us";
+          DefaultIM = "mozc";
+        };
+        "Group/0/Items/0".Name = "keyboard-us";
+        "Group/0/Items/1".Name = "mozc";
+      };
+    };
+
   };
 }
