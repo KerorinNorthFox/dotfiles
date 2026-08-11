bac7fcc55530f13332edaa25e8f08768fb5c6644
kerorinnf <kerorinnf@gmail.com>
2026-08-11 23:56:05 +0900

    feat(nixos): 認証システムのpolkitとkdeの認証エージェントを追加. 認証エージェントを常駐させる設定を追加.

--- nixos/modules/system/window-manager/niri/default.nix
+++ nixos/modules/system/window-manager/niri/default.nix
@@ -1,3 +1,36 @@
 {
+  pkgs,
+  ...
+}:
+
+{
   programs.niri.enable = true;
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gnome
+      pkgs.xdg-desktop-portal-gtk
+    ];
+  };
+
+  security.polkit.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    kdePackages.polkit-kde-agent-1
+  ];
+
+  systemd.user.services.polkit-kde-authentication-agent-1 = {
+    description = "polkit-kde-authentication-agent-1";
+    wantedBy = [ "graphical-session.target" ];
+    wants = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.kdePackages.polkit-kde-agent-1}/libexec/polkit-kde-authentication-agent-1";
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
+    };
+  };
 }
