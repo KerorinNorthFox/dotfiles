f4e85ddd199ca6c83ef5be3b1ebaa143298b8caf
kerorinnf <kerorinnf@gmail.com>
2026-08-12 16:07:10 +0900

    feat(nixos): swaynotificationcenterを導入.

--- nixos/home/kerorinnf/home.nix
+++ nixos/home/kerorinnf/home.nix
@@ -19,6 +19,7 @@
     ../../modules/user/file-manager/yazi
     ../../modules/user/monitoring/bottom
     ../../modules/user/monitoring/btop
+    ../../modules/user/notification/swaync
     ../../modules/user/search/rofi
     ../../modules/user/shell/fish
     ../../modules/user/status-bar/waybar
