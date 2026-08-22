c6f8ae2339921faa5a69bdb42ceccd81fb12e349
kerorinnf <kerorinnf@gmail.com>
2026-08-22 23:49:11 +0900

    feat(nixos/sddm): sddm-astronautのblack_holeテーマを改造.

--- nixos/modules/system/display-manager/sddm/default.nix
+++ nixos/modules/system/display-manager/sddm/default.nix
@@ -8,8 +8,45 @@ let
     (pkgs.sddm-astronaut.override {
       embeddedTheme = "black_hole";
       themeConfig = {
-        HeaderTextColor = "#b7c7d7";
+        Font = "FiraMono Nerd Font Mono";
+
         Background = "Backgrounds/sddm-wallpaper.jpg";
+
+        HeaderTextColor = "#93a7be";
+        DateTextColor = "#93a7be";
+        TimeTextColor = "#697f98";
+
+        FormBackgroundColor = "#d7ebf2";
+
+        LoginFieldBackgroundColor = "#fafeff";
+        PasswordFieldBackgroundColor = "#fafeff";
+        LoginFieldTextColor = "#697f98";
+        PasswordFieldTextColor = "#697f98";
+        UserIconColor = "#697f98";
+        PasswordIconColor = "#697f98";
+
+        PlaceholderTextColor = "#adc0d3";
+        WarningColor = "#eb8a8a";
+
+        LoginButtonTextColor = "#fafeff";
+        LoginButtonBackgroundColor = "#a1c9e8";
+        SystemButtonsIconsColor = "#697f98";
+        SessionButtonTextColor = "#697f98";
+        VirtualKeyboardButtonTextColor = "#697f98";
+
+        DropdownTextColor = "#fafeff";
+        DropdownSelectedBackgroundColor = "#a1c9e8";
+        DropdownBackgroundColor = "#b0d8f2";
+
+        HighlightTextColor = "#fbe4e8";
+        HighlightBackgroundColor = "#697f98";
+        HighlightBorderColor = "#fbe4e8";
+
+        HoverUserIconColor = "#fbe4e8";
+        HoverPasswordIconColor = "#fbe4e8";
+        HoverSystemButtonsIconsColor = "#fbe4e8";
+        HoverSessionButtonTextColor = "#fbe4e8";
+        HoverVirtualKeyboardButtonTextColor = "#fbe4e8";
       };
     }).overrideAttrs
       (oldAttrs: {
