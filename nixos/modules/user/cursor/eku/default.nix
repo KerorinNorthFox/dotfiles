35c1c7310b0c722b9f158223c0a657e3faa37d90
kerorinnf <kerorinnf@gmail.com>
2026-08-30 06:18:28 +0900

    fix(nixos): 自前でカーソルを用意する場合pointerCursorはいらないので削除.

--- nixos/modules/user/cursor/eku/default.nix
+++ nixos/modules/user/cursor/eku/default.nix
@@ -4,14 +4,6 @@
       ".local/share/icons/Eku".source = ../../../../assets/cursors/Eku;
     };
 
-    pointerCursor = {
-      enable = true;
-      gtk.enable = true;
-      x11.enable = true;
-      name = "Eku";
-      size = 16;
-    };
-
     sessionVariables = {
       XCURSOR_THEME = "Eku";
       XCURSOR_SIZE = "16";
