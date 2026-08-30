f29a1cda6a561781b9c0b8ee6bc2523d4cd82dad
kerorinnf <kerorinnf@gmail.com>
2026-08-31 02:29:37 +0900

    fix(nixos): kritaでボタンが押せない, キャンバス上でカーソルが動かない等のwayland上で発生するqtの問題が発生したため, QT_QPA_PLATFORM=waylandで常に起動するようkritaのビルドを変更.

--- nixos/modules/user/editor/image/krita.nix
+++ nixos/modules/user/editor/image/krita.nix
@@ -3,8 +3,18 @@
   ...
 }:
 
+let
+  kritaWayland = pkgs.symlinkJoin {
+    name = "krita-wayland";
+    paths = [ pkgs.krita ];
+    nativeBuildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/krita --set QT_QPA_PLATFORM wayland
+    '';
+  };
+in
 {
   home.packages = [
-    pkgs.krita
+    kritaWayland
   ];
 }
