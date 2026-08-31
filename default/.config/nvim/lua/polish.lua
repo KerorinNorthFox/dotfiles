58e04d1bf8b267034739d256c41fd742f30980c1
kerorinnf <kerorinnf@gmail.com>
2026-09-01 02:47:53 +0900

    feat(nvim): 背景透過.

--- default/.config/nvim/lua/polish.lua
+++ default/.config/nvim/lua/polish.lua
@@ -1,4 +1,10 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- Transparent background.
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "NonText", { bg = "none" })
+vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "none" })
 
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
