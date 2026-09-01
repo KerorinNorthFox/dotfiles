911498e09e8ab805f3e715cd8a58308b4126fbd6
kerorinnf <kerorinnf@gmail.com>
2026-09-01 09:10:15 +0900

    fix(fish): imagemagick等の他コマンドと被るためオリジナルのコマンドの名前変更.

--- default/.config/fish/user/ffmpeg_compress.fish
+++ default/.config/fish/user/ffmpeg_compress.fish
@@ -1,24 +1,24 @@
 function compress
     if test (count $argv) -lt 2
         echo "Usage: compress input_file level --encoder"
-        echo "OPTIONS"
+        echo OPTIONS
         echo "--encoder [h264|h265]"
         return 1
     end
-    
+
     set encoder_cpu libx264 # h264 for cpu. high compatibility, uncompressed, low-capacity.
     set encoder_gpu h264_nvenc # h264 for gpu. high compatibility, uncompressed, low-capacity.
 
     set i 3
     while test $i -le (count $argv)
         switch $argv[$i]
-            case "--encoder"
+            case --encoder
                 set encoder $argv[(math $i + 1)]
-                if test $encoder = "h265"
+                if test $encoder = h265
                     set encoder_cpu libx265
                     set encoder_gpu hevc_nvenc
                     echo "[INFO]: Video codec is set as h265."
-                else if test $encoder = "h264"
+                else if test $encoder = h264
                     echo "[INFO]: Video codec is set as h264."
                 else
                     echo "[WARN]: Available video codec is h264 or h265. h264 is applied as default."
@@ -31,9 +31,9 @@ function compress
                 continue
         end
     end
-    
+
     echo "[INFO]: Encoder for CPU - '$encoder_cpu', Encoder for GPU - '$encoder_gpu'"
-    
+
     set -l input $argv[1]
     set -l rate $argv[2]
     set -l output (string join _ (path change-extension '' $input) "compress" "$rate.mp4")
