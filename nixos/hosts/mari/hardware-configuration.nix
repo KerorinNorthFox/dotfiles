1d834d5c2424219ea41aa6ad1d9c4c223bafaf64
kerorinnf <kerorinnf@gmail.com>
2026-09-11 10:19:32 +0900

    feat(nixos/mari): ノートpcの固有ハードウェア設定を記述.

--- nixos/hosts/mari/hardware-configuration.nix
+++ nixos/hosts/mari/hardware-configuration.nix
@@ -9,9 +9,43 @@
 
 {
   imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+
     inputs.nixos-hardware.nixosModules.common-cpu-intel
     inputs.nixos-hardware.nixosModules.common-pc-laptop
     inputs.nixos-hardware.nixosModules.common-pc-laptop-ssd
   ];
-  # TODO: nixosインストール後のhardware-configuration.nixを追記.
+
+  boot.initrd.availableKernelModules = [
+    "xhci_pci"
+    "nvme"
+    "usb_storage"
+    "usbhid"
+    "sd_mod"
+  ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  hardware.graphics.enable = true;
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/1efc2339-0422-42e7-aa43-2e49e56bae3f";
+    fsType = "ext4";
+  };
+
+  # ESPを移行したためdeviceのuuidを変更済み.
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/5E78-610D";
+    fsType = "vfat";
+    options = [
+      "fmask=0022"
+      "dmask=0022"
+    ];
+  };
+
+  swapDevices = [ ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
