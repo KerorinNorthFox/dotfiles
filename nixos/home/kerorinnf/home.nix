1f9f07d15b252c46ae7044c7cfedee37f7c08624
kerorinnf <kerorinnf@gmail.com>
2026-09-11 12:24:02 +0900

    feat(nixos): modules/userの構成を変更.

--- nixos/home/kerorinnf/home.nix
+++ nixos/home/kerorinnf/home.nix
@@ -9,29 +9,29 @@
 
 {
   imports = [
-    ../../modules/user/audio/qpwgraph.nix
-    ../../modules/user/browser/ani-cli.nix
-    ../../modules/user/browser/vivaldi.nix
+    ../../modules/user/ani-cli
     ../../modules/user/buildtool/make.nix
     ../../modules/user/cli
     ../../modules/user/cursor/eku
-    ../../modules/user/desktop-shell/notcalia
+    ../../modules/user/editor/gimp.nix
+    ../../modules/user/editor/kid3.nix
+    ../../modules/user/editor/krita.nix
+    ../../modules/user/editor/losslesscut.nix
     ../../modules/user/editor/neovim
-    ../../modules/user/editor/image/gimp.nix
-    ../../modules/user/editor/image/krita.nix
-    ../../modules/user/editor/music/kid3.nix
-    ../../modules/user/editor/video/losslesscut.nix
     ../../modules/user/file-manager/dolphin
     ../../modules/user/file-manager/yazi
     ../../modules/user/monitoring/bottom
     ../../modules/user/monitoring/btop
+    ../../modules/user/noctalia
     ../../modules/user/notification/libnotify.nix
+    ../../modules/user/qpwgraph.nix
     ../../modules/user/shell/fish
     ../../modules/user/terminal/foot
     ../../modules/user/terminal/zellij
     ../../modules/user/viewer/image
     ../../modules/user/viewer/music
     ../../modules/user/viewer/video
+    ../../modules/user/vivaldi
   ];
 
   home = {
