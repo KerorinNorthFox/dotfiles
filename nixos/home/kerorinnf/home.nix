78efd5ad69bc11b662481b77109d0e9c1c24305a
kerorinnf <kerorinnf@gmail.com>
2026-09-14 17:10:55 +0900

    feat(nixos): hyprpickerを追加.

--- nixos/home/kerorinnf/home.nix
+++ nixos/home/kerorinnf/home.nix
@@ -26,6 +26,7 @@ in
     ../../modules/user/file-manager/dolphin
     (import ../../modules/user/file-manager/yazi { })
     ../../modules/user/hyprlock.nix
+    ../../modules/user/hyprpicker.nix
     ../../modules/user/monitoring/bottom
     ../../modules/user/monitoring/btop
     ../../modules/user/noctalia
