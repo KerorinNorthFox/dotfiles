6e8efd7ef4c50b621f068fa4342ebe154e72ffd3
kerorinnf <kerorinnf@gmail.com>
2026-09-16 02:56:03 +0900

    feat(nixos/hibiki): hardware-configuration.nixを追加.

--- nixos/hosts/hibiki/hardware-configuration.nix
+++ nixos/hosts/hibiki/hardware-configuration.nix
@@ -9,12 +9,64 @@
 
 {
   imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+
     inputs.nixos-hardware.nixosModules.common-cpu-amd
     inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
     inputs.nixos-hardware.nixosModules.common-cpu-amd-zenpower
-    inputs.nixos-hardware.nixosModules.common-gpu-nvidia
-    inputs.nixos-hardware.nixosModules.common-gpu-nvidia-sync
     inputs.nixos-hardware.nixosModules.common-pc-ssd
   ];
-  # TODO: nixosインストール後のhardware-configuration.nixを追記.
+
+  boot.initrd.availableKernelModules = [
+    "nvme"
+    "xhci_pci"
+    "ahci"
+    "usbhid"
+    "usb_storage"
+    "sd_mod"
+  ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  hardware.graphics.enable = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia = {
+    open = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    modesetting.enable = true;
+    powerManagement.enable = true;
+    nvidiaSettings = true;
+  };
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/bab6f9c1-dd3b-440d-80f3-e537f5f73303";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/82D2-00C8";
+    fsType = "vfat";
+    options = [
+      "fmask=0077"
+      "dmask=0077"
+    ];
+  };
+
+  # Dドライブ(2tb hdd)
+  fileSystems."/mnt/d" = {
+    device = "/dev/disk/by-uuid/38164D7E164D3DDE";
+    fsType = "ntfs";
+  };
+
+  # Eドライブ(2tb ssd)
+  fileSystems."/mnt/e" = {
+    device = "/dev/disk/by-uuid/C83255EF3255E2CC";
+    fsType = "ntfs";
+  };
+
+  swapDevices = [ ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
