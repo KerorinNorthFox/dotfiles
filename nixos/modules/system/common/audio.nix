9c66c76c3ae294038bd5dee4516062d71b0517d8
kerorinnf <kerorinnf@gmail.com>
2026-09-16 20:36:05 +0900

    feat(nixos): usb, audioデバイス関係のツールを導入.

--- nixos/modules/system/common/audio.nix
+++ nixos/modules/system/common/audio.nix
@@ -17,5 +17,6 @@
   environment.systemPackages = with pkgs; [
     pulseaudio
     pavucontrol
+    alsa-utils
   ];
 }
--- nixos/modules/system/common/common.nix
+++ nixos/modules/system/common/common.nix
@@ -13,5 +13,8 @@
     vim
     wget
     zip
+
+    usbutils
+    pciutils
   ];
 }
